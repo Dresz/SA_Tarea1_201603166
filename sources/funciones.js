cb5f451fcf6cf2421e458149e74dd5eff6fdf87d
Dresz <43056279+Dresz@users.noreply.github.com>
2020-08-08 10:25:08 -0600

    Prueba de acceso al token

--- sources/funciones.js
+++ sources/funciones.js
@@ -1,5 +1,4 @@
-const Url = 'https://api.softwareavanzado.world/index.php?webserviceClient=administrator'
-    +'&webserviceVersion=1.0.0&option=contact&api=hal'
+const Url = 'https://api.softwareavanzado.world/index.php?option=token&api=oauth2&grant_type=client_credentials&client_id=sa&client_secret=fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745'
 function insertar() {
     let parametros = {
         name: document.getElementById("nombre").value
