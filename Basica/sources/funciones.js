533ccf9293d6df302caf7317697754007b812e77
Dresz <43056279+Dresz@users.noreply.github.com>
2020-08-13 01:14:51 -0600

    Parte 2

--- Basica/sources/funciones.js
+++ Basica/sources/funciones.js
@@ -1,76 +1,94 @@
 
 
 function insertar() {
-    const Url = 'https://api.softwareavanzado.world/index.php?webserviceClient=administrator'
-    +'&webserviceVersion=1.0.0&option=contact&api=hal'
-    let parametros = {
-        name: document.getElementById("nombre").value
-    }
-   
-   
-    $.ajax({
-        url:Url,
-        type: 'Post',
-        data: JSON.stringify(parametros),
-        headers: {'Authorization': 'Bearer '+token},
-        success: function(result){
-            console.log(result)
-        },
-        error: function(error){
-            console.log(token)
+
+    var user = 'sa';
+    var pass = 'usac';
+    
+    
+        var xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.open('POST', 'https://api.softwareavanzado.world/administrator/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap', true);
+        xmlhttp.setRequestHeader ("Authorization", "Basic " + btoa(user + ":" + pass));
+        // build SOAP request
+        var sr =
+       ' <Envelope xmlns="http://www.w3.org/2003/05/soap-envelope">'+
+        '<Body>'+
+            '<create xmlns="https://api.softwareavanzado.world/media/redcore/webservices/joomla/administrator.contact.1.0.0.wsdl">'+
+               ' <name xmlns="">'+document.getElementById("nombre").value+'</name>'+
+            '</create>'+
+            '</Body>'+
+    '</Envelope>';
+
+        xmlhttp.onreadystatechange = function () {
+            
+            if (xmlhttp.readyState == 4) {
+                if (xmlhttp.status == 200) {
+                    
+                    console.log('Listo!');
+                }
+            }
         }
-    });
+        // Send the POST request
+        xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+        xmlhttp.send(sr);
+        // send request
+        // ...
 
 }
 
 function obtener() {
-    var GUrl = 'https://api.softwareavanzado.world/index.php?webserviceClient=administrator'
-    +'&webserviceVersion=1.0.0&option=contact&api=hal'
-    +'&list[limit]=0&filter[search]=201603166'
-    setTimeout(() => {  
-    $.ajax({
-        url:GUrl,
-        type: 'Get',
-        headers: {'Authorization': 'Bearer '+token} ,
-        success: function(result){
-            var ob = result._embedded.item
-            console.log(ob)
+    
+
+    var user = 'sa';
+    var pass = 'usac';
+    
+    
+        var xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.open('POST', 'https://api.softwareavanzado.world/administrator/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap', true);
+        xmlhttp.setRequestHeader ("Authorization", "Basic " + btoa(user + ":" + pass));
+        // build SOAP request
+        var sr =
+        '<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope">'+
+        '<Body>'+
+           ' <readList xmlns="https://api.softwareavanzado.world/media/redcore/webservices/joomla/administrator.contact.1.0.0.wsdl">'+
+                '<limitStart xmlns="">0</limitStart>'+
+                '<limit xmlns="">0</limit>'+
+                '<filterSearch xmlns="">201603166</filterSearch>'+
+               
+                
+          '</readList>'+
+        '</Body>'+
+    '</Envelope>';
+
+        xmlhttp.onreadystatechange = function () {
             
-            ob.forEach(element => {
+            if (xmlhttp.readyState == 4) {
+                if (xmlhttp.status == 200) {
+                    
+                    parser = new DOMParser();
+                    xmlDoc = parser.parseFromString(xmlhttp.responseText,"text/xml");
+                    // alert('done. use firebug/console to see network response');
+                    var ob =xmlDoc.getElementsByTagName("name");
+                    var id = xmlDoc.getElementsByTagName("id");
+                    
+            
+                    for (i = 0; i< ob.length; i++) {
                 var newRow=document.getElementById('customers').insertRow();
-                newRow.innerHTML="<td>"+element.id+"</td><td>"+element.name+"</td>"; 
-            });
-            
-        },
-        error: function(error){
-            console.log(token)
+                newRow.innerHTML="<td>"+id[i].childNodes[0].nodeValue+"</td><td>"+ob[i].childNodes[0].nodeValue+"</td>"; 
+            };
+                }
+            }
         }
-    });}, 300);
+        // Send the POST request
+        xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+        xmlhttp.send(sr);
+        // send request
+        // ...
+    
 
 }
-let token = "";
-function obtener2() {
-    var GUrl = 'https://api.softwareavanzado.world/index.php?option=token&api=oauth2';
-    let parametrosToken = 
-    {
-        grant_type:"client_credentials",
-        client_id:"sa",
-        client_secret:"fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745",
-    }
-    $.ajax({
-        url:GUrl,
-        type: 'Post',
-        data: parametrosToken,
-        success: function(result){
-            token = result.access_token+"";
-        },
-        error: function(error){
-            console.log(error)
-        }
-    });
-
-}
-obtener2()
 
 obtener()
 
